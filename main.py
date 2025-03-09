6ad267c81275480bb9720362b8b7ccc97a49af9e
keasky <142187674+keasky@users.noreply.github.com>
2025-03-09 12:23:41 +0900

    firest comit

---
+++ main.py
@@ -0,0 +1,37 @@
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from google import genai
+from fastapi.middleware.cors import CORSMiddleware
+import dotenv 
+import os
+
+dotenv.load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def gemini(message):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents=message,
+    )
+    return response.text
+@app.get("/")
+def read_root():
+    # return {"message": "Hello, FastAPI!"}
+    # リクエスト形式を指定してハローワールドと書いたHTMLを返す　
+    return HTMLResponse(content="<h1>Hello, FastAPI!</h1>", status_code=200)
+
+@app.get("/gemini/{message}")
+def read_item(message: str):
+    return gemini(message)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
