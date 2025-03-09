1d6e3876acc1849b8d6893ada43c86fd45cfdb50
keasky <142187674+keasky@users.noreply.github.com>
2025-03-09 13:06:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from google import genai
 from fastapi.middleware.cors import CORSMiddleware
+import dotenv 
 import os
 
+from fastapi.templating import Jinja2Templates
+
+dotenv.load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 app = FastAPI()
 app.add_middleware(
@@ -15,21 +20,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# テンプレートのディレクトリを指定
+app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
 def gemini(message):
     response = client.models.generate_content(
         model="gemini-2.0-flash", contents=message,
     )
     return response.text
-@app.get("/")
-def read_root():
-    # return {"message": "Hello, FastAPI!"}
-    # リクエスト形式を指定してハローワールドと書いたHTMLを返す　
-    return HTMLResponse(content="<h1>Hello, FastAPI!</h1>", status_code=200)
 
 @app.get("/gemini/{message}")
 def read_item(message: str):
     return gemini(message)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
